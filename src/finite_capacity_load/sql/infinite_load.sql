1dc2df209a5cad81af4cc511b6bb19d1b534589d
Adam Harper <github@adam-harper.com>
2014-12-12 10:18:15 +0000

    Sum load per order/op to a single entry on the "start date"
    Rather than having entries for an order/op split over multiple
    days (dictated by the "infinite" capacity of the work centre)
    we just want the total duration that we need to schedule and
    the earliest date we should schedule from.
    
    (Also convert load value to minutes.)

--- src/finite_capacity_load/sql/infinite_load.sql
+++ src/finite_capacity_load/sql/infinite_load.sql
@@ -1,5 +1,5 @@
 select
-  l.work_day,
+  min(l.work_day) start_date,
   l.contract,
   l.part_no,
   'M' bom_type_db,
@@ -10,21 +10,23 @@ select
   null release_no,
   null sequence_no,
   l.operation_no,
-  (l.op_finish_time - l.op_start_time) load_hours,
-  3 rnk,
-  rank() over (
-    order by
-      l.work_day,
-      l.op_start_time,
-      l.op_finish_time
-  ) subrnk
+  round(sum(l.op_finish_time - l.op_start_time) * 60, 0) duration,
+  3 rnk
 from ifsapp.crp_mach_op_load l
 where work_center_no = :work_center_no
+group by
+  l.contract,
+  l.part_no,
+  l.routing_revision_no,
+  l.routing_alternative_no,
+  l.crp_source_db,
+  l.counter,
+  l.operation_no
 --
 union
 --
 select
-  l.work_day,
+  min(l.work_day) start_date,
   l.contract,
   so.part_no,
   so.order_code_db,
@@ -35,24 +37,30 @@ select
   l.release_no,
   l.sequence_no,
   l.operation_no,
-  l.hours_loaded load_hours,
-  decode(so.objstate, 'Planned', 2, 1) rnk,
-  rank() over (
-    order by
-      l.work_day,
-      l.order_no,
-      l.release_no,
-      l.sequence_no,
-      l.operation_no
-  ) subrnk
+  round(sum(l.hours_loaded) * 60, 0) duration,
+  decode(so.objstate, 'Planned', 2, 1) rnk
 from ifsapp.mach_operation_load l
 join ifsapp.shop_ord so
   on l.order_no = so.order_no
   and l.release_no = so.release_no
   and l.sequence_no = so.sequence_no
 where work_center_no = :work_center_no
+group by
+  l.contract,
+  so.part_no,
+  so.order_code_db,
+  so.routing_revision,
+  so.routing_alternative,
+  so.objstate,
+  l.order_no,
+  l.release_no,
+  l.sequence_no,
+  l.operation_no
 --
 order by
-  work_day,
+  start_date,
   rnk,
-  subrnk
+  order_no,
+  release_no,
+  sequence_no,
+  operation_no
