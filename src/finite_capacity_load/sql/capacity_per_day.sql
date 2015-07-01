ee6aab4a504c96c3ead86af50c3d5e2566b6508d
Adam Harper <github@adam-harper.com>
2015-07-01 09:49:01 +0100

    ~80% performance improvement to capacity per day query

--- src/finite_capacity_load/sql/capacity_per_day.sql
+++ src/finite_capacity_load/sql/capacity_per_day.sql
@@ -14,38 +14,73 @@ select
       0
     )
   ), 0) capacity,
-  round(sum(
-    nvl(
-      ifsapp.work_center_int_api.work_center_maint_load(
-        wtc.work_day, wc.contract, wc.work_center_no
-      ),
-      0
-    ) * 60
-  ), 0) maintenance,
-  round(sum(
-    nvl(
-      wtc.working_time
-      * (ra.efficiency / 100)
-      * (wc.utilization / 100),
-      0
+  round(sum((
+    select
+      nvl(
+        wtc.working_time
+        * (ra.efficiency / 100)
+        * (wc.utilization / 100),
+        0
+      )
+      * least(count(*), 1)
+    from ifsapp.active_work_order awo
+    join ifsapp.operational_status os
+      on awo.op_status_id = os.op_status_id
+    where wcr.contract = awo.contract
+      and wcr.mch_code = awo.mch_code
+      and awo.objstate in ('UNDERPREPARATION','PREPARED','STARTED','RELEASED')
+      and os.operational_status_type_db = '1'
+      and wtc.work_day between awo.plan_s_date and awo.plan_f_date
+  )), 0) maintenance,
+  greatest(
+    0,
+    (
+      round(sum(
+        nvl(
+          wtc.working_time
+          * (ra.efficiency / 100)
+          * (wc.utilization / 100),
+          0
+        )
+      ), 0)
+      -
+      round(sum((
+        select
+          nvl(
+            wtc.working_time
+            * (ra.efficiency / 100)
+            * (wc.utilization / 100),
+            0
+          )
+          * least(count(*), 1)
+        from ifsapp.active_work_order awo
+        join ifsapp.operational_status os
+          on awo.op_status_id = os.op_status_id
+        where wcr.contract = awo.contract
+          and wcr.mch_code = awo.mch_code
+          and awo.objstate in ('UNDERPREPARATION','PREPARED','STARTED','RELEASED')
+          and os.operational_status_type_db = '1'
+          and wtc.work_day between awo.plan_s_date and awo.plan_f_date
+      )), 0)
     )
-    - nvl(
-      ifsapp.work_center_int_api.work_center_maint_load(
-        wtc.work_day, wc.contract, wc.work_center_no
-      ),
-      0
-    ) * 60
-  ), 0) capacity_available
+  ) capacity_available
 from ifsapp.work_center wc
+--
 join ifsapp.work_time_counter wtc
   on wc.calendar_id = wtc.calendar_id
+--
+join ifsapp.work_center_resource wcr
+  on wc.contract = wcr.contract
+  and wc.work_center_no = wcr.work_center_no
 left outer join ifsapp.work_center_resource_avail ra
-  on wc.contract = ra.contract
-  and wc.work_center_no = ra.work_center_no
+  on wcr.contract = ra.contract
+  and wcr.work_center_no = ra.work_center_no
+  and wcr.resource_id = ra.resource_id
   and ra.operable_db = 'Y'
   and wtc.work_day between
     ra.begin_date and
     nvl(ra.end_date, to_date('9999-12-31', 'yyyy-mm-dd'))
+--
 where wc.work_center_no = :work_center_no
   and wtc.work_day >= trunc(sysdate)
 group by
@@ -54,5 +89,4 @@ group by
   wc.calendar_id,
   wtc.work_day
 order by
-  wc.work_center_no,
   wtc.work_day
