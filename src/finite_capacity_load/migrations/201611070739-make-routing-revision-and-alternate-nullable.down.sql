fbe1be9ecb4c2bdd8bcb0a0b8fef454315afe86e
Adam Harper <github@adam-harper.com>
2016-11-07 07:43:09 +0000

    Make routing revision and alternate nullable
    Shop Orders _can_ be raised without a specified routing revision and
    alternate. When such orders exist the generation fails because the
    corresponding load cannot be inserted into the `finite_load` table due
    to the routing revision and alternate columns being marked `not null`.

---
+++ src/finite_capacity_load/migrations/201611070739-make-routing-revision-and-alternate-nullable.down.sql
@@ -0,0 +1,1 @@
+alter table finite_load modify (routing_revision_no varchar2(4) not null, routing_alternative_no varchar2(20) not null);
---
+++ src/finite_capacity_load/migrations/201611070739-make-routing-revision-and-alternate-nullable.up.sql
@@ -0,0 +1,1 @@
+alter table finite_load modify (routing_revision_no varchar2(4) null, routing_alternative_no varchar2(20) null);
